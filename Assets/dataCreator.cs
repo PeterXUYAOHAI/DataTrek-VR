ec9ca7baa9b856ba1cffe23ed44b36d9c5c94182
PeterYAOHAIXU <yaohaixupeter@gmail.com>
2017-02-27 11:34:22 +0800

    finish demo

--- Assets/dataCreator.cs
+++ Assets/dataCreator.cs
@@ -8,14 +8,18 @@ public class dataCreator : MonoBehaviour {
 
 	public GameObject ball;
 	public GameObject cube;
+	public GameObject capsule;
 
 
 	// Use this for initialization
 	void Start () {
+		float scale = 50;
+		int number = 800;
 		Random rnd = new Random ();
-		for (int i = 0; i < 100; i++) {
-			Instantiate (ball, new Vector3 ((float)(101 + rnd.NextDouble()*5), (float)(101 + rnd.NextDouble()*5), (float)(101 + rnd.NextDouble()*5)), Quaternion.identity);
-			Instantiate(cube, new Vector3 ((float)(99 + rnd.NextDouble()*5), (float)(99 + rnd.NextDouble()*5), (float)(99 + rnd.NextDouble()*5)), Quaternion.identity);
+		for (int i = 0; i < number; i++) {
+			Instantiate (ball, new Vector3 ((float)(101 + rnd.NextDouble()*scale), (float)(101 + rnd.NextDouble()*scale), (float)(101 + rnd.NextDouble()*scale)), Quaternion.identity);
+			Instantiate(cube, new Vector3 ((float)(99 + rnd.NextDouble()*scale), (float)(99 + rnd.NextDouble()*scale), (float)(99 + rnd.NextDouble()*scale)), Quaternion.identity);
+			Instantiate(capsule, new Vector3 ((float)(95 + rnd.NextDouble()*scale), (float)(70 + rnd.NextDouble()*scale), (float)(70 + rnd.NextDouble()*scale)), Quaternion.identity);
 		}
 
 
