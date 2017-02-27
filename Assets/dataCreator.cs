f8c7e619de0d77a8a29caf2bd8b84b6ba81e5f4d
PeterYAOHAIXU <yaohaixupeter@gmail.com>
2017-02-27 11:21:49 +0800

    finish basic coding

--- Assets/dataCreator.cs
+++ Assets/dataCreator.cs
@@ -1,19 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class dataCreator : MonoBehaviour {
 
 	public GameObject ball;
+	public GameObject cube;
+
 
 	// Use this for initialization
 	void Start () {
-		
-//		Instantiate (ball, Vector3 (101, 101, 101), Vector3(0,0,0));
+		Random rnd = new Random ();
+		for (int i = 0; i < 100; i++) {
+			Instantiate (ball, new Vector3 ((float)(101 + rnd.NextDouble()*5), (float)(101 + rnd.NextDouble()*5), (float)(101 + rnd.NextDouble()*5)), Quaternion.identity);
+			Instantiate(cube, new Vector3 ((float)(99 + rnd.NextDouble()*5), (float)(99 + rnd.NextDouble()*5), (float)(99 + rnd.NextDouble()*5)), Quaternion.identity);
+		}
+
+
+	//	Instantiate (ball, transform.position, transform.rotation);
+
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+			
 	}
 }
